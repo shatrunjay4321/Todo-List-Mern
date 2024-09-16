968448ba874b3c3a2654b3300cc97537105fab82
shatrunjay4321 <shatrunjay4321@gmail.com>
2024-09-16 19:56:20 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -77,6 +77,7 @@ const App = () => {
   };
 
   const handleMarkAll = (todos) => {
+    setMarkAll((prev) => !prev);
       todos.forEach((todo) => {
         const editTask = async (todo) => {
           try {
